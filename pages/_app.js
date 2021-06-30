ede126aeb68a1aedc2484fb3158d9ca18089d4fd
Somsubhra Das <das.somsubhra1@gmail.com>
2021-06-30 22:20:52 +0530

    code cleaned up

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,12 @@
-import '../styles/globals.css'
+import "../styles/globals.css";
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      hello
+      <Component {...pageProps} />
+    </>
+  );
 }
 
-export default MyApp
+export default MyApp;
