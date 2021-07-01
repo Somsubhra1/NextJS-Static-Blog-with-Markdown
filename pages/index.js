a89744b7e0f2072302ad7563231db4d2e0bd1221
Somsubhra Das <das.somsubhra1@gmail.com>
2021-07-01 10:09:33 +0530

    file names with markdown meta data fetched

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,8 @@
 import Head from "next/head";
+import fs from "fs";
+import path from "path";
 
-export default function Home() {
+export default function Home({ posts }) {
   return (
     <div>
       <Head>
@@ -9,3 +11,30 @@ export default function Home() {
     </div>
   );
 }
+
+export const getStaticProps = async () => {
+  // Get files from posts directory
+  const files = fs.readdirSync(path.join("posts"));
+
+  // Get slug and frontmatter from posts
+  const posts = files.map((filename) => {
+    // create slug
+    const slug = filename.replace(".md", "");
+
+    // Get frontmatter
+    const markdownWithMeta = fs.readFileSync(
+      path.join("posts", filename),
+      "utf-8"
+    );
+    console.log(markdownWithMeta);
+
+    return { slug };
+  });
+
+  console.log(posts);
+  return {
+    props: {
+      posts: "The posts",
+    },
+  };
+};
